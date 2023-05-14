ea74dee15d9d3b32300aa9a57fff827dc055abda
Mkparu <mkparusomtochi26@gmail.com>
2023-05-14 01:19:13 +0100

    wrote types for product and categories

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,4 +1,14 @@
+type categories = "men's clothing" | "women's clothing" | "accessories" | "electronics";
 interface product{
-    id:number,
-    
+    id:number;
+    title:string;
+    price:number;
+    category:categories;
+    image:string;
+    rating:{
+        rate:number;
+        count:number
+    }
 }
+
+export type {categories, product}
